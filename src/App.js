10a695af9fae659bcd3df94fdab7b1f110d343f8
AppVeyor <ci@appveyor.com>
2022-06-23 23:14:45 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
-import React, { useState, useRef } from 'react';
-import ClassCounter from './Components/ClassCounter';
-import Counter from './Components/Counter';
-import PostItem from './Components/PostItem';
+import React, {useMemo, useState } from 'react';
+import PostFilter from './Components/PostFilter';
+import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
 import MyButton from './Components/UI/button/MyButton';
-import MyInput from './Components/UI/input/MyInput';
+import MyModal from './Components/UI/MyModal/MyModal';
 import './styles/App.css'
  
 function App() {
   
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
-    {id: 2, title: 'Javascript 2', body: 'Description'},
-    {id: 3, title: 'Javascript 3', body: 'Description'},
+    {id: 1, title: 'fff', body: 'Description'},
+    {id: 2, title: 'aa 2', body: 'll'},
+    {id: 3, title: 'bb 3', body: 'kk'},
   ])
 
-  const [post, setPost] = useState({title: '', body: ''});
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false);
 
-  const addNewPost = (e) => {
-     e.preventDefault();
-     setPosts([...posts, {...post, id: Date.now()}]);
-     setPost({title: '', body: ''})
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort])) 
+    }
+    return posts;
+  }, [filter.sort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  }, [filter.query, sortedPosts])
+
+  const createPost = (newPost) => {
+     setPosts([...posts, newPost])
+     setModal(false)
+  }
+
+  // Получаем post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
   }
 
   return (
     <div className="App">
-      <form>
-        {/* Управляемый компонент */}
-        <MyInput 
-            value={post.title} 
-            onChange={e => setPost({...post, title: e.target.value})}
-            type="text" 
-            placeholder="Название поста"
-        />
-        {/* Неуправляемый компонент */}
-        <MyInput 
-            value={post.body} 
-            onChange={e => setPost({...post, body: e.target.value})}
-            type="text" 
-            placeholder="Описание поста"
-        />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title="Посты про JS"/>
+      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+         <PostForm create={createPost}/>
+      </MyModal>
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
     </div>
   );
 }
