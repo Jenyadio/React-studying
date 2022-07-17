b35fd4be0ca0cfd77c521f19d6d47f4cc5fb6a90
AppVeyor <ci@appveyor.com>
2022-07-17 17:25:30 +0300

    forth

--- src/App.js
+++ src/App.js
@@ -1,61 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import PostService from './API/PostService';
-import PostFilter from './Components/PostFilter';
-import PostForm from './Components/PostForm';
-import PostList from './Components/PostList';
-import MyButton from './Components/UI/button/MyButton';
-import MyModal from './Components/UI/MyModal/MyModal'
-import Loader from './Components/UI/loader/Loader'
-import {usePosts} from './hooks/usePosts';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './Components/UI/navbar/Navbar';
 import './styles/App.css'
-import { useFetching } from './hooks/useFetching';
+import AppRouter from './Components/AppRouter';
  
 function App() {
-  
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostsLoading, postError ] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
-  })
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
-  const createPost = (newPost) => {
-     setPosts([...posts, newPost])
-     setModal(false)
-  }
-
-  // Получаем post из дочернего компонента
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
 
   return (
-    <div className="App">
-      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-         <PostForm create={createPost}/>
-      </MyModal>
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}
-      />
-      {postError &&
-        <h1>Произошла ошибка ${postError}</h1>
-      }
-      {isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
-      }
-    </div>
+    <BrowserRouter className="App">
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error() {
+    return (
+        <h1 style={{color: 'red'}}>
+            Вы перешли на несуществующую страницу!
+        </h1>
+    )
+}
